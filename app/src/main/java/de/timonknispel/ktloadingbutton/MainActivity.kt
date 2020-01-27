0b6929f2898a3b965ef6ae92ebf2f170e627792f
Timon Knispel <t.knispel@appfarms.com>
2020-01-27 17:17:59 +0100

    added new views

--- app/src/main/java/de/timonknispel/ktloadingbutton/MainActivity.kt
+++ app/src/main/java/de/timonknispel/ktloadingbutton/MainActivity.kt
@@ -3,34 +3,30 @@ package de.timonknispel.ktloadingbutton
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     private val tag = this.javaClass.simpleName
 
+    val handler = Handler()
+
+    var prog = 0F
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        test_button.setProgress(50F)
-        test_button.setOnClickListener {
-            initWithLiveData().observe(this, Observer {
-                when (it) {
-                    NetStatus.SUCCESS -> test_button.doResult(true) {
-                        // do some other stuff when animation is done
-                    }
-                    NetStatus.ERROR -> test_button.doResult(false) {
-                        // do some other stuff when animation is done
-                    }
-                    else -> test_button.startLoading()
-                }
-            })
-        }
+        initWithLiveData().observe(this, Observer {
+            test_button.setProgress(it)
+            if (it == 100F) {
+                handler.removeCallbacksAndMessages(null)
+                test_button.doResult(true)
+            }
+        })
 
         // when touch is needed even if the button is in loading state (e.g. cancel loading on click)
         test_button.touchListener = {
@@ -39,22 +35,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     // test function for generating some data for the button
-    private fun initWithLiveData(): MutableLiveData<NetStatus> {
-        val liveData = MutableLiveData<NetStatus>()
+    private fun initWithLiveData(): MutableLiveData<Float> {
+        val liveData = MutableLiveData<Float>()
+
 
         // first set state to loading
-        liveData.value = NetStatus.LOADING
+        liveData.value = prog
 
-        // later set the result
-        Handler().postDelayed({
-            // post the new value
-            if (Random.nextBoolean()) {
-                liveData.postValue(NetStatus.SUCCESS)
-            } else {
-                liveData.postValue(NetStatus.ERROR)
+        val runnableCode = object : Runnable {
+            override fun run() {
+                prog += 1F
+                liveData.postValue(prog)
+                handler.postDelayed(this, 50)
             }
-        }, Random.nextLong(2000, 4000))
+        }
 
+        handler.post(runnableCode)
         return liveData
     }
 }
